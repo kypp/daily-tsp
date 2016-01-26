4c71e5d8300b9a15f27ee6418bc4b466f53b0556
Krzysztof Skrzynecki <krzysztof@skrzynecki.net>
2016-01-26 21:07:21 +0100

    GRASP algorithm added (nondeterministic - don't forget to srand())

--- types.h
+++ types.h
@@ -3,6 +3,7 @@
 
 using city_t = int;
 using route_t = std::list<city_t>;
+using routev_t = std::vector<city_t>;
 using distance_t = float;
 using graph_t = std::vector<std::vector<distance_t>>;
 using days_t = std::vector<route_t>;
